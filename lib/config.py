42093a7f5320d66df79b0a92bf30d0c660a8bdf6
binaryflesh <logan.campos123@gmail.com>
2018-12-01 05:06:09 -0600

    Updated .gitignore. Default on github ignores 'lib/' .

---
+++ lib/config.py
@@ -0,0 +1,47 @@
+# Configparser to scan config files
+# 
+# Copyright (c) 2018 Logan Campos - @binaryflesh
+
+# imports
+import os
+import sys
+run_Path = os.path.dirname(os.path.realpath(__file__))
+
+import configparser
+import json
+    
+class Configuration():
+    ConfigParser = configparser.ConfigParser()
+    ConfigParser.read(os.path.join(run_Path, "../etc/config.ini"))
+    default = {"UmlBaseUrl": "https://www.omg.org/spec/UML/", "UmlVersion": "2.5.1"}
+
+    @classmethod
+    def readSetting(cls, section, item, default):
+        result = default
+        try:
+            if isinstance(default, bool):
+                result = cls.ConfigParser.getboolean(section, item)
+            elif isinstance(default, int):
+                result = cls.ConfigParser.getint(section, item)
+            else:
+                result = cls.ConfigParser.get(section, item)
+        except:
+            pass
+        finally:
+            return result
+
+    @classmethod
+    def getUMLBaseUrl(cls) -> str:
+        return cls.readSetting("UML", "baseUrl", cls.default["UmlBaseUrl"])
+    
+    @classmethod
+    def getUMLVersion(cls) -> str:
+        return cls.readSetting("UML", "version", cls.default["UmlVersion"])
+
+    @classmethod
+    def parseUMLdataset(cls) -> dict:
+        data = None
+        with open("../etc/sources.json") as f:
+            data = json.load(f)
+            f.close()
+        return data
